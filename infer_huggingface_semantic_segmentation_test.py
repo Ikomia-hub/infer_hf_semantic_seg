3b4c25e33ca121578e58d21f19ca5b8ff3719bf4
Allan <allan.kouidri@gmail.com>
2022-11-22 11:59:38 +0100

    Add test file

---
+++ infer_huggingface_semantic_segmentation_test.py
@@ -0,0 +1,18 @@
+import logging
+import cv2
+from ikomia.core import task
+from ikomia.utils.tests import run_for_test
+
+
+logger = logging.getLogger(__name__)
+
+
+def test(t, data_dict):
+    logger.info("===== Test::infer Hugging Face semantic segmentation=====")
+    params = task.get_parameters(t)
+    params["model_name_or_path"] = "nvidia/segformer-b5-finetuned-ade-640-640"
+    img = cv2.imread(data_dict["images"]["detection"]["coco"])[::-1]
+    input_img_0 = t.getInput(0)
+    input_img_0.setImage(img)
+    task.set_parameters(t, params)
+    yield run_for_test(t)
