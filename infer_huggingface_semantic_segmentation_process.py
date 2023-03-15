dc6485588d86004628d3e803295ef4c4f4305eac
Allan <allan.kouidri@gmail.com>
2023-03-15 17:10:59 +0100

    Edit text model info

--- infer_huggingface_semantic_segmentation_process.py
+++ infer_huggingface_semantic_segmentation_process.py
@@ -206,7 +206,7 @@ class InferHuggingFaceSemanticSegmentationFactory(dataprocess.CTaskFactory):
         self.info.shortDescription = "Semantic segmentation using models from Hugging Face."
         self.info.description = "This plugin proposes inference for semantic segmentation"\
                                 "using transformers models from Hugging Face. It regroups"\
-                                "models covered by the Hugging Face class:"\
+                                "models covered by the Hugging Face class: "\
                                 "AutoModelForSemanticSegmentation. Models can be loaded either"\
                                 "from your fine-tuned model (local) or from the Hugging Face Hub."
         # relative path -> as displayed in Ikomia application process tree
